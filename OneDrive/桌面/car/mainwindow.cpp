ffdc590993da397709942d158ce1bbb33d40b889
Xavier <xavier970808@gmail.com>
2025-03-31 06:56:55 +0800

    尚有誤

--- OneDrive/桌面/car/mainwindow.cpp
+++ OneDrive/桌面/car/mainwindow.cpp
@@ -1,6 +1,8 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include<QTimer>
+#include<QDebug>
+#include<QKeyEvent>
 
 /*
  *0 => init 遊戲被開啟時
@@ -22,10 +24,10 @@ MainWindow::MainWindow(QWidget *parent): QMainWindow(parent), ui(new Ui::MainWin
     object_timer->start(10);    //每 10 毫秒觸發
 
     //更新時間
-    time = 0;
+    time = 30;
     clock_timer = new QTimer(this);    //建立計時器
     connect(clock_timer, SIGNAL(timeout()), SLOT(update_time()));   //更新時間
-    clock_timer->start(1000);   //每1000毫秒更新一次
+    //clock_timer->start(1000);   //每1000毫秒更新一次
 }
 
 MainWindow::~MainWindow()
@@ -55,4 +57,56 @@ void MainWindow::update_object(){
     if(bgm_pos == -32){
         bgm_pos = 0;
     }
+
+    if(game_status == 1){
+        //playing
+
+        car_distance += 1;    //增加移動距離
+        ui->label_distance->setText("行駛距離: " + QString::number(car_distance, 'f', 0) + "公尺");   //顯示移動距離
+        move_car();
+    }
 }
+
+void MainWindow::move_car(){
+    int car_new_pos = car_pos + car_direction;  //將接下來要移動到的位置暫存起來
+    if((car_new_pos) >= 30 && (car_new_pos) <= 410){
+        car_pos = car_new_pos;
+        ui->car->setGeometry(QRect(10, car_pos, 237, 71));  //設定車子垂直位置
+    }
+    else{
+        game_status = 4;
+        game_stop();
+    }
+}
+
+void MainWindow::on_button_start_game_clicked()
+{
+    game_start();
+}
+
+void MainWindow::game_start(){
+    game_status = 1;    //設定playing狀態
+    ui->label_title->setVisible(false);    //隱藏遊戲標題
+    ui->button_start_game->setVisible(false);   //隱藏遊戲開始按鈕
+    clock_timer->start(1000);   //每隔1000毫秒更新一次
+    time = 30;  //重設遊戲時間
+    ui->lcd_clock->display(time);   //顯示遊戲時間
+
+    car_pos = 220;  //重設車子的垂直位置(位於中央)
+    car_distance = 0;   //重設移動距離
+}
+
+void MainWindow::keyPressEvent(QKeyEvent *event){
+    switch(event->key()){
+    case Qt::Key_Up:
+        car_direction = -1;
+        qDebug() << "up";
+        break;
+    case Qt::Key_Down:
+        car_direction = 1;
+        qDebug() << "down";
+        break;
+
+    }
+}
+
--- OneDrive/桌面/car/mainwindow.h
+++ OneDrive/桌面/car/mainwindow.h
@@ -17,15 +17,23 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+protected:
+    void keyPressEvent(QKeyEvent *);
+
 private slots:
     void update_time();    //遊戲時間更新
     void update_object();   //遊戲物體移動更新
+
+    void on_button_start_game_clicked();
 
 private:
     Ui::MainWindow *ui;
     int time;   //遊戲時間
     int game_status;     //遊戲狀態
     int bgm_pos;    //背景位置(水平)
+    int car_pos;    //車子位置(垂直)
+    int car_direction;  //車子方向(垂直)
+    float car_distance;    //車子移動距離
 
     QTimer *clock_timer;    //遊戲時間計時器
     QTimer *object_timer;   //物體移動計時器
@@ -33,5 +41,6 @@ private:
     void game_start();
     void game_pause();
     void game_stop();
+    void move_car();
 };
 #endif // MAINWINDOW_H
